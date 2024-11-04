446dd8ea2c42d21ca5cf7c55feaae8903732cf7a
= <mortaiwasnotallowed@gmail.com>
2024-11-05 08:53:18 +1030

    testing redirect on connect

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("io game yes yes");
+
+window.onload = function() {
+    console.log("the document has loadednated");
+
+    const connect_button = document.getElementById("connectbutton");
+    const username_input = document.getElementById("usernameinput");
+    const gameselect = document.getElementById("gameselect");
+
+    connect_button.onclick = function() {
+        const username = username_input.value;
+        const game = gameselect.options[gameselect.selectedIndex].text;
+
+        window.location.href = "/slither.html";
+
+        console.log("Joining game: " + game + " with username: " + username);
+    }
+}
