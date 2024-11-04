8321732fb13c4f1926c49edd14a88c61240aa58b
= <mortaiwasnotallowed@gmail.com>
2024-11-05 09:10:13 +1030

    added username when redirecting

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ window.onload = function() {
         const username = username_input.value;
         const game = gameselect.options[gameselect.selectedIndex].value;
 
-        window.location.href = "/IO/" + game + ".html";
+        window.location.href = "/IO/" + game + ".html?u=" + username;
 
         console.log("Joining game: " + game + " with username: " + username);
     }
